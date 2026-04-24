f7bedfd9ac210e94d2fbc8909a6cf26e3d35d0ef
Mohammed Shoaib <mohammedshoaib.connect@gmail.com>
2026-04-24 09:21:32 +0530

    Backend setup

--- backend/db.js
+++ backend/db.js
@@ -3,14 +3,14 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const db = new pg.Client({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+const { Pool } = pg;
+
+const db = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-db.connect();
 
 export default db;
